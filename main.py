f21d2b82934eeaa01e55fc0abcc376ae46619ba1
reniboyanova <reni91boyanova@gmail.com>
2023-02-06 13:22:45 +0200

    Starting project commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# In this game you need to guess which of the names is richer
+# For example 'Richard Branson' vs 'Ilon Musk' ---> type 'higher' if you think the second is richer, or 'lower' if
+# you think the second is poorer
+
+
+the_compare_names_values = {
+
+}
+
+
